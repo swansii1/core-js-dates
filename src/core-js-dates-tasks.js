36dcec9c260efed9fce238c77e78b3b1481f58cd
Matvey <matvey.swansii@gmail.com>
2025-03-24 18:44:49 +0300

    feat: getDayName solved, other issues fixed

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -20,8 +20,7 @@
 function dateToTimestamp(date) {
   const dateArg = new Date(date);
 
-  const time = dateArg.getTime();
-  return time;
+  return dateArg.getTime();
 }
 
 /**
@@ -34,8 +33,11 @@ function dateToTimestamp(date) {
  * Date(2023, 5, 1, 8, 20, 55) => '08:20:55'
  * Date(2015, 10, 20, 23, 15, 1) => '23:15:01'
  */
-function getTime(/* date */) {
-  throw new Error('Not implemented');
+function getTime(date) {
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+  return `${hours}:${minutes}:${seconds}`;
 }
 
 /**
@@ -49,8 +51,21 @@ function getTime(/* date */) {
  * '03 Dec 1995 00:12:00 UTC' => 'Sunday'
  * '2024-01-30T00:00:00.000Z' => 'Tuesday'
  */
-function getDayName(/* date */) {
-  throw new Error('Not implemented');
+function getDayName(date) {
+  const day = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ];
+  const now = new Date(date);
+
+  const dayIndex = now.getUTCDay();
+
+  return day[dayIndex];
 }
 
 /**
